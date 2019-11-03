c5bb69a5578b2c4f4aa20ead54af1845b0737409
akanehl <akanehl@ucsc.edu>
2019-11-03 15:06:27 -0800

    init push
    Cool floaty ghosts

---
+++ CMPM170Proto-2/Assets/Floater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+
+    Vector3 posOffset = new Vector3();
+    Vector3 temporary = new Vector3();
+
+    public float amplitude;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        temporary = posOffset;
+        temporary.y += Mathf.Sin(Time.fixedTime * Mathf.PI * speed) * amplitude;
+
+        transform.position = temporary;
+    }
+}
