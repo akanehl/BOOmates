58ed3716cd7ea3b5648a5405cbd761e97b7ac903
akanehl <akanehl@ucsc.edu>
2019-11-03 15:59:29 -0800

    Shitty movement
    Shitty movement update

---
+++ CMPM170Proto-2/Assets/GhostMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+
+    public Rigidbody rigBod;
+    public float moveSpeed;
+
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 temporary = new Vector3();
+
+    public float amplitude;
+    public float speed;
+
+    public string AxisNameHoriz;
+    public string AxisNameVert;
+
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        temporary = posOffset;
+        temporary.y += Mathf.Sin(Time.fixedTime * Mathf.PI * speed) * amplitude;
+
+        transform.position = temporary;
+
+        float moveHorizontal = Input.GetAxis(AxisNameHoriz);
+        float moveVertical = Input.GetAxis(AxisNameVert);
+
+        //Debug.Log(moveHorizontal + " " + moveVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //Debug.Log(movement);
+
+        rigBod.AddForce(movement * moveSpeed);
+
+   
+    }
+}
