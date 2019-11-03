250f693c84d4b6ff78ad12d62b8d403f54563d64
akanehl <akanehl@ucsc.edu>
2019-11-03 15:21:52 -0800

    Discord??
    Does this work???

---
+++ CMPM170Proto-2/Assets/Ghost Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+
+    public Rigidbody rigBod;
+    public float moveSpeed;
+
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
