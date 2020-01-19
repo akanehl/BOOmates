bf9edb56cb70fc0ddc727d5acd83182b55e0822a
aniken998 <enzhou@ucsc.edu>
2020-01-19 15:37:08 -0800

    work with controller on Human

---
+++ BOOmates/Assets/Scripts/HumanBehavior.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HumanBehavior : MonoBehaviour
+{
+    public enum HumanState {NORMAL, CONTROL}
+    private HumanState currentState;
+
+    [SerializeField]
+    private float speed;
+
+    public Transform[] moveSpots; 
+    private int randomSpot;
+
+    private float waitTime;
+
+    [SerializeField]
+    private float startWaitTime;
+
+    MyHumanController myControls;
+
+    Vector2 myMove;
+
+    void Awake()
+    {
+        myControls = new MyHumanController();
+        myControls.GamePlay.TakeBody.performed += context => TakeBody();
+        myControls.GamePlay.LeaveBody.performed += context => LeaveBody();
+
+        myControls.GamePlay.MyMovement.performed += context => myMove = context.ReadValue<Vector2>();
+        myControls.GamePlay.MyMovement.canceled += context => myMove = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+        currentState = HumanState.NORMAL;
+        waitTime = startWaitTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(currentState == HumanState.NORMAL)
+        {
+            normalBehavior();
+        }
+        else
+        {
+            movement();
+        }
+    }
+
+    //Human AI, move around when human isn't control by ghost(player)
+    void normalBehavior()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, 
+                                                    speed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(moveSpots[randomSpot].position);
+        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0) 
+            {
+                int prevRandomSpot = randomSpot;
+                while (prevRandomSpot == randomSpot)
+                {
+                    randomSpot = Random.Range(0, moveSpots.Length);
+                }
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+    void movement()
+    {
+        Vector3 moveVec = new Vector3(-myMove.x, 0.0f ,-myMove.y);
+        transform.Translate(moveVec * speed * Time.deltaTime, Space.World);
+        if(moveVec != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(new Vector3(-myMove.x, 0 ,-myMove.y));
+        }
+    }
+
+    void OnEnable()
+    {
+        myControls.GamePlay.Enable();
+    }
+
+    void OnDisable()
+    {
+        myControls.GamePlay.Disable();
+    }
+
+    //take the body/take control by player
+    void TakeBody()
+    {
+        Debug.Log("Control");
+        currentState = HumanState.CONTROL;
+        
+    }
+
+    //leave the body/back to AI
+    void LeaveBody()
+    {
+        currentState = HumanState.NORMAL;
+        Debug.Log("Normal");
+    }
+}
---
+++ BOOmates/Assets/Scripts/MyHumanController.cs
@@ -0,0 +1,190 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/MyHumanController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @MyHumanController : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public @MyHumanController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""MyHumanController"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""aa05514f-da86-4741-907a-61020a5fb6b6"",
+            ""actions"": [
+                {
+                    ""name"": ""TakeBody"",
+                    ""type"": ""Button"",
+                    ""id"": ""af101261-6061-4372-9d4d-df8aa87ce609"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""LeaveBody"",
+                    ""type"": ""Button"",
+                    ""id"": ""26f314dd-eaf3-4e42-8e59-da1bdaf4b90f"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MyMovement"",
+                    ""type"": ""Button"",
+                    ""id"": ""29442f6d-4014-4d43-8ad7-74dce50a9ca1"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8c2d6810-3e3d-455c-9deb-bcc92d7ba1a4"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""TakeBody"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3530d95f-92b0-4a3d-8eb7-8fcc3c3abe3d"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LeaveBody"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9e8936be-7c84-45be-b950-9b4fa13f9b0e"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MyMovement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_TakeBody = m_GamePlay.FindAction("TakeBody", throwIfNotFound: true);
+        m_GamePlay_LeaveBody = m_GamePlay.FindAction("LeaveBody", throwIfNotFound: true);
+        m_GamePlay_MyMovement = m_GamePlay.FindAction("MyMovement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_TakeBody;
+    private readonly InputAction m_GamePlay_LeaveBody;
+    private readonly InputAction m_GamePlay_MyMovement;
+    public struct GamePlayActions
+    {
+        private @MyHumanController m_Wrapper;
+        public GamePlayActions(@MyHumanController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @TakeBody => m_Wrapper.m_GamePlay_TakeBody;
+        public InputAction @LeaveBody => m_Wrapper.m_GamePlay_LeaveBody;
+        public InputAction @MyMovement => m_Wrapper.m_GamePlay_MyMovement;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @TakeBody.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnTakeBody;
+                @TakeBody.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnTakeBody;
+                @TakeBody.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnTakeBody;
+                @LeaveBody.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnLeaveBody;
+                @LeaveBody.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnLeaveBody;
+                @LeaveBody.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnLeaveBody;
+                @MyMovement.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMyMovement;
+                @MyMovement.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMyMovement;
+                @MyMovement.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMyMovement;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @TakeBody.started += instance.OnTakeBody;
+                @TakeBody.performed += instance.OnTakeBody;
+                @TakeBody.canceled += instance.OnTakeBody;
+                @LeaveBody.started += instance.OnLeaveBody;
+                @LeaveBody.performed += instance.OnLeaveBody;
+                @LeaveBody.canceled += instance.OnLeaveBody;
+                @MyMovement.started += instance.OnMyMovement;
+                @MyMovement.performed += instance.OnMyMovement;
+                @MyMovement.canceled += instance.OnMyMovement;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    public interface IGamePlayActions
+    {
+        void OnTakeBody(InputAction.CallbackContext context);
+        void OnLeaveBody(InputAction.CallbackContext context);
+        void OnMyMovement(InputAction.CallbackContext context);
+    }
+}
