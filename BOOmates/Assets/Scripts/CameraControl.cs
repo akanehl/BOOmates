2ff5ba2a89fd9c0af737df29467b509f3b35701c
Ragneroke <46947503+Ragneroke@users.noreply.github.com>
2020-01-22 23:36:21 -0800

    Adding the respawning mechanic

--- BOOmates/Assets/Scripts/CameraControl.cs
+++ BOOmates/Assets/Scripts/CameraControl.cs
@@ -10,22 +10,24 @@ public class CameraControl : MonoBehaviour
 {
     // Start is called before the first frame update
     public  float   moveSpeed = 20f;
-    public int     cameraCondition = 2;
-    private Vector3 FirstRoom;
+    public  int     cameraCondition = 2;
+    //private Vector3 FirstRoom;
     private Vector3 SecondRoom;
-    private Vector3 ThirdRoom;
-    private Vector3 ForthRoom;
+    //private Vector3 ThirdRoom;
+    //private Vector3 ForthRoom;
     public  Vector3 TargetRoom;
-    public bool    isMoving = false;
+    public  Vector3 RespawnPoint;
+    public  bool    isMoving = false;
     public  float   smoothTime = 0.3F;
     private Vector3 velocity = Vector3.zero;
-    public GameObject player;
+    public  GameObject player1;
+    public  GameObject player2;
     void Start()
     {
-        FirstRoom = new Vector3(0f,10f,-7.3f);
+        //FirstRoom = new Vector3(0f,10f,-7.3f);
         SecondRoom = new Vector3(13.8f,10f,-7.3f);
-        ThirdRoom = new Vector3(-15.5f,10f,-7.3f);
-        ForthRoom = new Vector3(-15.5f,10f,-22f);
+        //ThirdRoom = new Vector3(-15.5f,10f,-7.3f);
+        //ForthRoom = new Vector3(-15.5f,10f,-22f);
         transform.position = SecondRoom;
     }
 
@@ -33,43 +35,21 @@ public class CameraControl : MonoBehaviour
     void Update()
     {
         if (isMoving){
-        	CameraTransform(cameraCondition);
+        	CameraTransform(TargetRoom);
         }
         //Debug.Log(isMoving);
     }
 
-    void CameraTransform(int camera){
-    	var TargetCamera = new Vector3();
-        switch (camera)
-        {
-            case 1:
-                TargetCamera = FirstRoom;
-                break;
-            case 2:
-                TargetCamera = SecondRoom;
-                break;
-            case 3:
-                TargetCamera = ThirdRoom;
-                break;
-            case 4:
-                TargetCamera = ForthRoom;
-                break;
-        }
-
-    	transform.position = Vector3.SmoothDamp(transform.position, TargetCamera, ref velocity, smoothTime);
-    	if(transform.position == TargetCamera){
+    void CameraTransform(Vector3 TargetRoom){
+    	transform.position = Vector3.SmoothDamp(transform.position, TargetRoom, ref velocity, smoothTime);
+        playerRespawn();
+    	if(transform.position == TargetRoom){
     		isMoving = false;
     	}
     }
 
-    // void checkRoom(){
-    //     if(player.transform.position.x < (transform.position.x - 7) && cameraCondition == 1){
-    //         cameraCondition = 3;
-    //         isMoving = true;
-    //     }
-    //     if(player.transform.position.x > (transform.position.x + 7) && cameraCondition == 1){
-    //         cameraCondition = 2;
-    //         isMoving = true;
-    //     }
-    // }
+    void playerRespawn(){
+        // player1.transform.position = RespawnPoint;
+        // player2.transform.position = RespawnPoint;
+    }
 }
--- BOOmates/Assets/Scripts/DoorManagement.cs
+++ BOOmates/Assets/Scripts/DoorManagement.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class DoorManagement : MonoBehaviour
 {
 	public GameObject human;
+	private GameObject player1;
+	private GameObject player2;
 	private GameObject mainCamera;
 	public GameObject doorWay;
 	private int cameraCondition;
+	private CameraControl sc;
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = GameObject.Find("MainCamera");
-        CameraControl sc = mainCamera.GetComponent<CameraControl>();
-        Debug.Log(mainCamera); 
+        player1 = GameObject.Find("Ghost_1");
+        player2 = GameObject.Find("Ghost_2");
+        sc = mainCamera.GetComponent<CameraControl>();
     }
 
     // Update is called once per frame
@@ -24,24 +28,20 @@ public class DoorManagement : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
     	if(other.gameObject.name == "Ghost_2"){
-    		CameraControl sc = mainCamera.GetComponent<CameraControl>();
-    		if(doorWay.transform.parent.name == "Room1"){
-    			sc.cameraCondition = 1;
-
-    		}else if(doorWay.transform.parent.name == "Room2"){
-    			sc.cameraCondition = 2;
-    		
-
-    		}else if(doorWay.transform.parent.name == "Room3"){
-    			sc.cameraCondition = 3;
-    			
-    			
-    		}else if(doorWay.transform.parent.name == "Room4"){
-    			sc.cameraCondition = 4;
-    			
-    			
+    		var parentRoom = doorWay.transform.parent.gameObject;
+    		var TargetRoom = parentRoom.transform.Find("cameraPos").gameObject.transform.position;
+    		var RespawnPoint = parentRoom.transform.Find("RespawnPoint").gameObject.transform.position;
+    		sc.TargetRoom = TargetRoom;
+    		// if(sc.isMoving){
+    		// 	player1.transform.position = RespawnPoint;
+      		//  player2.transform.position = RespawnPoint;
+    		// }
+    		if(TargetRoom != sc.transform.position){
+    			sc.RespawnPoint = parentRoom.transform.position;
+    			sc.isMoving = true;
+    			player1.transform.position = RespawnPoint;
+      		    player2.transform.position = RespawnPoint;
     		}
-    		sc.isMoving = true;
     	}
     }
 }
