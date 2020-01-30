7f00a5105ecf3764072e82db1af434efd0d51cf7
Jordan Timm <jordantimm090@gmail.com>
2020-01-29 16:44:10 -0800

    Basic Main Menu
    Initial main menu script and setup, waiting on furthur design instructions.

---
+++ BOOmates/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LoadLevel(string lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+    public void LoadLevel(int lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
